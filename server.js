09440c40c6bd5aec2f45288da689528b8d57f31b
Daniel Sochor <danielsochor@gmail.com>
2019-09-10 14:41:57 -0500

    creation of server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+);
