cb936f6f9c5179578c0b2361554d2188148b03ef
Daniel Sochor <danielsochor@gmail.com>
2019-09-10 15:06:04 -0500

    gitignore and package created

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const cors = require("cors");
+//const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
