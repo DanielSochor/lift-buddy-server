5c868ef3769c2d4a97564fcbc7d51dde5fd353e5
Daniel Sochor <danielsochor@gmail.com>
2019-09-13 12:33:04 -0500

    comment out '*' and change to static build

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(express.json());
 app.use(cors());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static('client/build'));
+  app.use(express.static('build'));
 }
 
-app.get('*', (request,response => {
-    response.sendFile(path.join(__dirname, 'client/build','index.html'));
-}));
+//app.get('*', (request,response => {
+    //response.sendFile(path.join(__dirname, 'client/build','index.html'));
+//}));
 
 // Add routes, both API and view
 // add the route directly here for now
