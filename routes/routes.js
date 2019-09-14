3cbac1c93e502d68609961b51b8e2bdb9cb1925e
Daniel Sochor <danielsochor@gmail.com>
2019-09-14 15:30:52 -0500

    remove extra npm command

--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,12 @@ module.exports = function (app) {
 
     //displays during view of backend
     app.get('/', (request, response) => {
-        response.json('ab');
+        response.json('just the back end for me');
     });
     
     //displays during button press
     app.get('/api/user', (request, response) => {
-        response.json('ac');
+        response.json('just the front end for me');
     });
 
 }; 
