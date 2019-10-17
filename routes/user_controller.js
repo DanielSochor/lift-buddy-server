e1c9dbbf673a27763a414494b0a6503ecf697a6c
Daniel Sochor <danielsochor@gmail.com>
2019-10-17 11:17:11 -0500

    don't redefine request

--- routes/user_controller.js
+++ routes/user_controller.js
@@ -2,17 +2,43 @@ let users = require('./user_ORM_functions');
 
 let user = {
     create: function (request, response) {
-        console.log('step 1');
-        let userRequest = {
-            email_address: 'test',
-        };
-        users.insertNew(userRequest, function (error, result) {
+        //Request has as lot of items in it and doesn't appear useful to show
+
+        //console.log('request is: ' + request);
+        //console.log(JSON.stringify(request));
+        //console.log('step 1');
+        //let userRequest = {
+        //    email_address: 'Test',
+        //
+
+        //try using request instead od new user request
+        //let userRequest = request;
+
+
+
+        //the request creates a lot of junk
+        //console.log('user request object key is: ' + Object.keys(userRequest)[0]);
+        //console.log('user request object value is: ' + Object.values(userRequest)[0]);
+
+        //console.log(userRequest);
+        users.insertNew(request, function (error, result) {
             if (error) {
                 console.log(error);
             } else {
+                console.log('users.insert New call works');
                 response.json({
-                    email: userRequest.email_address,
+                    email: request.email_address,
                 });
+                console.log('response is: ');
+                //Investigate this for where the null value comes from, why is email above shown as null
+                console.log(response._events);
+                console.log(response.email);
+                console.log(response);
+                console.log('response body is');
+                console.log(response.header);
+                //console.log(response.IncomingMessage.body);
+                //console.email is showing undefined
+                console.log('does the above value show as null?');
             }
         });
     }
