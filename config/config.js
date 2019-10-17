2132ea67df2be51376bbed0552005d690fdfbc7d
Daniel Sochor <danielsochor@gmail.com>
2019-10-17 11:07:10 -0500

    test if changes to config file affect backend or frontend working

--- config/config.js
+++ config/config.js
@@ -14,3 +14,16 @@
 //};
 
 //module.exports = config[process.env.APP_ENV || 'local'];
+
+module.exports = {
+    'development':{
+        'host': process.env.DB_HOST,
+        'username': process.env.DB_USER,
+        'password': process.env.DB_PASSWORD,
+        'database': process.env.DB_DATABASE,
+        'port': process.env.DB_PORT
+    },
+    'production':{
+        'use_ENV_variable': process.env.JAWSDB_URL
+    }
+}
