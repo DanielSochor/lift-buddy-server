2c94a26a43810d0c90e642bce2d0d694b2a1000d
Daniel Sochor <danielsochor@gmail.com>
2019-10-17 11:20:02 -0500

    removed lowercase command and commented items out

--- routes/user_ORM_functions.js
+++ routes/user_ORM_functions.js
@@ -2,11 +2,20 @@ let orm = require('./orm');
 
 let users = {
     insertNew: function(user, callback){
-        user.email = user.email.toLowerCase();
+        //console.log('xxxxx');
+        //console.log('user is: ' + user[0]);
+        //console.log('user is: ' + user[1]);
+        //console.log('test 0 key is: ' + Object.keys(user)[0]);
+        //console.log('test 0 value is: ' + Object.values(user)[0]);
+        //console.log('user is: ' + user);
+        //console.log('xxxxx');
+        //user.email = user.email_address.toLowerCase();
         let query = {
             table: 'users',
-            data: user
+            data: user //do the keys of this object match the table columns?
         };
+        console.log('query is: ' + query);
+        console.log('query data is: ' + query.data);
         orm.insert(query, callback);
     }
 
