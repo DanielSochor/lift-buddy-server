1e9708dffa318c212e00edb176c1698b3c22175c
Daniel Sochor <danielsochor@gmail.com>
2019-11-13 21:20:57 -0600

    added additional routes and ORM functions

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,6 @@
-var user = require("./user_controller");
+var user = require('./user_controller');
+var authorizer = require('./authenticate');
+
 
 module.exports = function (app) {
 
@@ -7,26 +9,47 @@ module.exports = function (app) {
         response.json('just the back end for me');
         //THIS IS CURRENTLY WHAT THE BACKEND SHOWS
     });
-    
-    app.get('/api/user', (request, response) => {
-        console.log('server called');
-        //This should show on heroku front end build
-        response.json('just the front end for me NOW');
+
+    //========Users====================================================================
+
+    app.post("/api/user", function(request, response) {
+        user.create(request, response);
+    });
+    app.post("/api/user/login", function(request, response) {
+        user.login(request, response);
+    });
+    app.delete("/api/user/login", function(request, response) {
+        user.logout(request, response);
+    });
+    app.get("/api/user", authorizer.authenticate, function(request, response) {
+        user.getUserBySession(request, response);
+    });
+    app.get("/api/user/:id", authorizer.authenticate, function(request, response) {
+        user.getUserByID(request, response);
     });
 
-    //login route
-    app.post('/api/user', (request, response) => {
-        //Request has as lot of items in it and doesn't appear useful to show
-        //console.log('route request is: ' + 'request has a lot of items in it');
-        //console.log(request);
-        //console.log('test 0 key is: ');
-        //console.log(Object.keys(request)[0]);
-        //console.log('test 0 value is: ');
-        //console.log(Object.values(request)[0]);
-        //response.json('just the front end for me now');
-        console.log('routes: app.post(/api/user) at routes is hit');
-        //how/where is this request created????
-        user.create(request,response)
-    });
+
+
+    
+    // app.get('/api/user', (request, response) => {
+    //     console.log('server called');
+    //     //This should show on heroku front end build
+    //     response.json('just the front end for me NOW');
+    // });
+
+    // //login route
+    // app.post('/api/user', (request, response) => {
+    //     //Request has as lot of items in it and doesn't appear useful to show
+    //     //console.log('route request is: ' + 'request has a lot of items in it');
+    //     //console.log(request);
+    //     //console.log('test 0 key is: ');
+    //     //console.log(Object.keys(request)[0]);
+    //     //console.log('test 0 value is: ');
+    //     //console.log(Object.values(request)[0]);
+    //     //response.json('just the front end for me now');
+    //     console.log('routes: app.post(/api/user) at routes is hit');
+    //     //how/where is this request created????
+    //     user.create(request,response)
+    // });
 
 }; 
--- routes/user_ORM_functions.js
+++ routes/user_ORM_functions.js
@@ -2,23 +2,59 @@ let orm = require('./orm');
 
 let users = {
     insertNew: function(user, callback){
-        //console.log('xxxxx');
-        //console.log('user is: ' + user[0]);
-        //console.log('user is: ' + user[1]);
-        //console.log('test 0 key is: ' + Object.keys(user)[0]);
-        //console.log('test 0 value is: ' + Object.values(user)[0]);
-        //console.log('user is: ' + user);
-        //console.log('xxxxx');
-        //user.email = user.email_address.toLowerCase();
         let query = {
             table: 'users',
             data: user //do the keys of this object match the table columns?
         };
-        //console.log('query is: ' + query);
         console.log('user_ORM_funtions: query data is: ' + query.data);
         orm.insert(query, callback);
+    },
+    selectByEmail: function(email, callback){
+        let query = {
+            table: 'users',
+            where: [{email: email.toLowerCase()}]
+        };
+        orm.select(query, callback);
+    },
+    selectByUsername: function(username, callback){
+        let query = {
+            table: 'users',
+            where: [{username: username}]
+        };
+        orm.select(query, callback);
+    },
+    updateSession: function(email, uuid, callback){
+        let query = {
+            table: 'users',
+            data: {session_token: uuid},
+            where: [{email: email.toLowerCase()}]
+        };
+        orm.update(query, callback);
+    },
+    removeSession: function(session, callback){
+        let query = {
+            table: 'users',
+            data: {session: null},
+            where: [{session_token: session}]
+        };
+        orm.update(query, callback);
+    },
+    getUserBySession: function(session, callback){
+        let query = {
+            table: 'users',
+            columns: ['email', 'id', 'created'],
+            where: [{session_token: session}]
+        };
+        orm.select(query, callback);
+    },
+    getUserByID: function(id, callback){
+        let query = {
+            table: 'users',
+            columns: ['email', 'id', 'created'],
+            where: [{id: id}]
+        };
+        orm.select(query, callback);
     }
-
 };
 
 module.exports = users;
