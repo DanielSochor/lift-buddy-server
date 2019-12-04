69ef8385677de0e8555224a7f9322a147b29b2f0
Daniel Sochor <danielsochor@gmail.com>
2019-12-03 21:33:46 -0600

    add utilities folder with constants.js file

---
+++ utilities/constants.js
@@ -0,0 +1,10 @@
+// Notification strings for pubsub
+export const NOTIF = {
+};
+
+// API routes
+export const API = {
+    login: 'api/user/login',
+    logout: 'api/user/logout',
+    signup: 'api/user/signup'
+};
