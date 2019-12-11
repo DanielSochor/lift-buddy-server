e2db05ecd49efb0df0cfac409a0d94bcc3c42a84
Daniel Sochor <danielsochor@gmail.com>
2019-12-11 15:02:22 -0600

    whitelisted cors websites appear to work with additional comments

--- server.js
+++ server.js
@@ -9,12 +9,27 @@ const PORT = process.env.PORT || 3001;
 const baseURL = (process.env.NODE_ENV === 'production') ? process.env.REACT_APP_SERVER_URL : process.env.REACT_APP_LOCAL_URL;
 //TODO: create an origin whitelist with both URLS
 
+//TODO: doesn't appear to like the trailing / in var whitelist = [process.env.REACT_APP_LOCAL_URL
+//TODO: doesn't like http://localhost:3001, may not like the lift-buddy-server
+var whitelist = ['http://localhost:3000', 'https://lift-buddy-server.herokuapp.com']
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 //app.use(cors());
 //app.use(cors({credentials: true}));
-app.use(cors({origin: 'http://localhost:3000'}));
+//app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors(corsOptions));
 //app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 
 // Serve up static assets (usually on heroku)
