0dd87e55c4c2d8bfb1e421b44e95f6dc6bdb3d18
Daniel Sochor <danielsochor@gmail.com>
2019-12-17 19:55:29 -0600

    Removal of unused routes

--- controllers/users.js
+++ controllers/users.js
@@ -175,113 +175,8 @@ let handleLogIn = (request, response, error, result) => {
 
 
 
-router.get('/api/users/signupX', (request, response) => {
-    response.json('api/users/signin hit');
-    console.log('api/users/signin hit');
-});
-
-// GET route for fetching one user by session token header or
-// all users by default
-router.get("/api/usersX", (request, response) => {
-    console.log('list all users');
-
-    if (request.headers['x-session-token']) {
-        console.log('cha cha cha');
-        console.log('request.headers[x-session-token]');
-        console.log(request.headers['x-session-token']);
-        user.selectWhere({ session_token: request.headers['x-session-token'] }, (error, result) => {
-            console.log('request headers in user.select where in users.js is: ');
-            console.log(request.headers);
-            if (result.length) {
-                response.status(200).json({ data: result });
-            } else {
-             response.status(404).json({ 'error': 'user not found' });
-            }
-        })
-    } else {
-        console.log('do not know what to tell you');
-    }
-        // user.selectUsersJoinChannels({ session_token: req.headers['x-session-token'] }, {}, (err, result) => {
-        //     if (result.length) {
-
-        //         let formatResult = formatUsersObject(result);
-
-        //         user.selectUsersJoinGroups({ session_token: req.headers['x-session-token'] }, formatResult, (err, result, params) => {
-        //             if (result.length) {
-        //                 result.forEach(element => {
-        //                     params.direct_messages.push({
-        //                         direct_group_id: element.direct_group_id
-        //                     });
-        //                 });
-        //             }
-        //             res.status(200).json(params);
-        //         });
-        //     } else {
-        //         res.status(404).json({ 'error': 'user not found' });
-        //     }
-        // });
-    // } else {
-    //     user.select((err, result) => {
-    //         res.status(200).json({ data: result });
-    //     });
-    // }
-});
-
-// GET route for fetching one user by ID
-router.get("/api/users/:id", (request, response) => {
-    console.log('retrieve user');
-
-    user.selectWhere({ user_id: request.params.id }, (err, result) => {
-        if (result.length) {
-            response.status(200).json(result[0]);
-        } else {
-            response.status(404).json({ 'error': 'user not found' });
-        }
-    })
-});
-
-// POST route for creating a user
-router.post("/api/users", (request, response) => {
-    if (!request.body.email_address.includes('@') || !request.body.email_address.includes('.')) {
-        response.status(400).json({ 'error': 'email is not valid' });
-    } else if (request.body.password !== request.body.password_confirm) {
-        response.status(400).json({ 'error': 'passwords do not match' });
-    } else {
-        let hashedPassword = hashpass(request.body.password);
-        let userRequest = {
-            first_name: request.body.first_name,
-            last_name: request.body.last_name,
-            email_address: request.body.email_address,
-            username: request.body.username,
-            // alias: req.body.alias,
-            password: hashedPassword.hash,
-            salt: hashedPassword.salt
-        };
-        console.log('userRequest for post at api/users is: ');
-        console.log(userRequest);
-        user.createUser(userRequest, (error, result) => {
-            if (error) {
-                console.log(error);
-                if (error.sqlMessage.includes('Duplicate')) {
-                    response.status(400).json({ 'error': 'email already exists in system' });
-                } else {
-                    response.status(500).json({ 'error': 'oops we did something bad' });
-                }
-            } else {
-                response.status(200).json({
-                    user_id: result.insertId,
-                    ///username: result.userame,
-                    email: userRequest.email_address
-                });
-                //console.log('res is: ');
-                //console.log(res);
-            }
-        });
-    }
-});
-
 // POST route for user log in
-router.post('/api/users/login', (request, response) => {
+router.post('/api/users/loginX', (request, response) => {
     //user can sign in with email or username
     console.log('req first item is: ');
     console.log(request[0]);
@@ -300,7 +195,7 @@ router.post('/api/users/login', (request, response) => {
 });
 
 // POST route for user log out
-router.delete('/api/users/login', (request, response) => {
+router.delete('/api/users/loginX', (request, response) => {
     user.update({ session_token: request.headers['x-session-token'] }, (error, result) => {
         result.status(200).json({ 'message': 'user logged out successfully' });
     });
