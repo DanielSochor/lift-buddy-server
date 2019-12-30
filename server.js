2c4a111012ef30c595744dc2d0f1355e4cf1a34f
Daniel Sochor <danielsochor@gmail.com>
2019-12-30 15:39:21 -0600

    add allowedHeaders: ['x-session-token'],

--- server.js
+++ server.js
@@ -34,9 +34,11 @@ app.use(express.json());
 //this should enable CORS preflight
 app.use(cors({
   credentials: true, 
-  exposedHeaders: ['Set-Cookie', 'Content-Length', 'Accept', 'X-Requested-With', 'X-HTTP-Method-Override', 'X-session-token' ],
+  exposedHeaders: ['Set-Cookie', 'Content-Length', 'Accept', 'X-Requested-With', 'X-HTTP-Method-Override', 'x-session-token' ],
   methods: ['GET', 'POST', 'OPTIONS', 'HEAD'],
+  allowedHeaders: ['x-session-token'],
 
+  //adding X-session-token to exposedHeaders didn't help
   //preflightContinue: false,
   //optionsSuccessStatus: 204,
   //origin: 'https://lift-buddy-client.herokuapp.Kcom/'
