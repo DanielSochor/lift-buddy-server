d068df5914d13ab1e8b06db6864b220b16d3c974
Daniel Sochor <danielsochor@gmail.com>
2020-01-06 12:49:24 -0600

    return connection to old configuration

--- config/db_connection.js
+++ config/db_connection.js
@@ -1,29 +1,29 @@
 //creates database connection
-var mysql = require('mysql');
+//var mysql = require('mysql');
 
-var config = require('./config');
+//var config = require('./config');
 
-var connection = mysql.createConnection(config.mysql.url);
+//var connection = mysql.createConnection(config.mysql.url);
 
-var connection = '';
+// var connection = '';
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    // Adding these process.env variables appears to work
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-    //host: 'localhost',
-    //port: 8889,
-    //user: 'root',
-    //password: 'root',
-    //database: 'fitness_buddy'
-  });
-};
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     // Adding these process.env variables appears to work
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE
+//     //host: 'localhost',
+//     //port: 8889,
+//     //user: 'root',
+//     //password: 'root',
+//     //database: 'fitness_buddy'
+//   });
+// };
 
 // connection.connect(function (err) {
 //   if (err) {
@@ -35,8 +35,8 @@ if (process.env.JAWSDB_URL) {
 
 // module.exports = connection;
 
-//var mysql = require("mysql");
-//var config = require('./config');
+var mysql = require("mysql");
+var config = require('./config');
 
 var connection = mysql.createConnection(config.mysql.url);
 connection.connect(function (error) {
