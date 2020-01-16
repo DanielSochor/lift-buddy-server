47f9b4d1eb42e0431d09103b9eb2e6336d1934b6
Daniel Sochor <danielsochor@gmail.com>
2020-01-16 12:44:24 -0600

    try using imported config file with full names instead of local conditional

--- config/db_connection.js
+++ config/db_connection.js
@@ -3,10 +3,10 @@ var config = require('./config');
 
 // var connection = '';
 
-if (process.env.NODE_ENV === 'production') {
-     connection = mysql.createConnection(process.env.JAWSDB_URL);
- } else {
-     connection = mysql.createConnection(process.env.DB_URL);
+//if (process.env.NODE_ENV === 'production') {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//     connection = mysql.createConnection(process.env.DB_URL);
      //    connection = mysql.createConnection({
      //        host: 'localhost',
      //        user: 'root',
@@ -14,11 +14,11 @@ if (process.env.NODE_ENV === 'production') {
      //        database: 'lift_buddy',
      //        port: 8889,
      //    }); 
- };
+// };
 
-//var connection = mysql.createConnection(config.mysql.url);
-
+var connection = mysql.createConnection(config.mysql.url);
 //var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
 connection.connect(function (error) {
     if (error) throw error;
     console.log("connected to database on " + connection.config.host + " as "
